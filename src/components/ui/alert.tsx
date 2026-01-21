daa2f2a2c6b8106dfd28e2cb4d535e8a5830b0fc
Dakshin A <autodesk.dakshin211@gmail.com>
2026-01-21 23:40:20 +0530

    Component change

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -40,4 +40,5 @@ const AlertDescription = React.forwardRef<HTMLParagraphElement, React.HTMLAttrib
 );
 AlertDescription.displayName = "AlertDescription";
 
+
 export { Alert, AlertTitle, AlertDescription };

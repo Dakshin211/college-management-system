f44f686b746b320f8e79a0f0a798bd149ffd106d
Dakshin A <autodesk.dakshin211@gmail.com>
2026-02-15 16:32:54 +0530

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -124,7 +124,7 @@ const Login = () => {
                 ) : (
                   'Sign In'
                 )}
-              </Button>}
+              </Button>
             <motion.div
               initial={{ opacity: 0, y: 10 }}
               animate={{ opacity: 1, y: 0 }}
